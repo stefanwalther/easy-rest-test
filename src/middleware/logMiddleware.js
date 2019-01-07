a93200ab405ca0df6c86a5ccf5dcbbf28eb0ccda
Stefan Walther <swr@qlik.com>
2019-01-07 22:02:49 +0100

    feat(logger): add body

--- src/middleware/logMiddleware.js
+++ src/middleware/logMiddleware.js
@@ -1,8 +1,10 @@
 const logger = require('winster').instance();
 
 function log(req, res, next) {
-  logger.trace('OK, we are logging a request');
+  logger.trace('----');
+  logger.trace('OK, we are logging a request:');
   logger.trace('req.headers', req.headers);
+  logger.trace('req.body', req.body || '<null>');
   next();
 }
 
