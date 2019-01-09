4298e691cb807d4734493c7352af8e492a117d07
Stefan Walther <swr@qlik.com>
2019-01-09 01:31:02 +0100

    feat(multple-tables): first attempt

--- src/modules/generic-dataset/generic-dataset.controller.js
+++ src/modules/generic-dataset/generic-dataset.controller.js
@@ -2,15 +2,13 @@ const expressResult = require('express-result');
 const logger = require('winster').instance();
 const path = require('path');
 const parse = require('csv-parse/lib/sync');
+const _ = require('lodash');
 
 const utils = require('./../../utils');
 
 class GenericDatasetController {
 
-  static async get(req, res) {
-    const dataset = req.params.name;
-    logger.trace('[GenericDatasetController.get]', dataset);
-
+  static async _getDataSet(dataset) {
     const filePath = path.resolve(__dirname, `./../../../data/${dataset}.csv`);
     let fileContent = await utils.readFile(filePath);
     const parseOptions = {
@@ -18,8 +16,18 @@ class GenericDatasetController {
       delimiter: ';',
       skip_empty_lines: true
     };
+    return parse(fileContent, parseOptions);
+  }
 
-    let records = parse(fileContent, parseOptions);
+  static async get(req, res) {
+    const datasets = _.split(_.trim(req.params.name), ',');
+    logger.trace('[GenericDatasetController.get]', datasets);
+
+    let records = await Promise.all(datasets.map(async item => {
+      logger.trace('item', item);
+      return {[item]: await GenericDatasetController._getDataSet(item)};
+    }));
+
     return expressResult.ok(res, {data: records});
   }
 
