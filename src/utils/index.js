439f5ba4899634986d1979b54c5a35849ed8c37f
Stefan Walther <swr@qlik.com>
2019-01-10 23:58:12 +0100

    feat: add  option

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,9 +4,11 @@ const util = require('util');
 async function readFile(filePath) {
   const readFile = util.promisify(fs.readFile);
   return readFile(filePath, 'utf-8');
-
 }
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 module.exports = {
-  readFile
+  readFile,
+  sleep
 };
