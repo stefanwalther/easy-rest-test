abe3782568b5e272352dda2eb17b218af0f8f77a
Stefan Walther <swr@qlik.com>
2019-01-18 00:24:14 +0100

    refactor: cleanup leftovers from express

--- src/middleware/logErrors.js
+++
@@ -1,8 +0,0 @@
-const logger = require('winster').instance();
-
-function logErrors(err, req, res, next) {
-  logger.trace(err.stack);
-  next(err);
-}
-
-module.exports = logErrors;
--- test/unit/hateoasify.js
+++
@@ -1,8 +0,0 @@
-
-describe('[unit] => Hateoasify', () => {
-  describe('format() => returns', () => {
-    it('`data` just passing a normal object');
-    it('`data` by just passing a string');
-    it('`data` by passing ');
-  });
-});
