b9760e8bdc4e641081ddf546b710fd7e87c2e8eb
Stefan Walther <swr@qlik.com>
2019-01-25 12:52:00 +0100

    feat: koa-body-wrap

--- src/initializers/100-favicon.js
+++ src/initializers/100-favicon.js
@@ -2,7 +2,7 @@ const favicon = require('koa-favicon');
 const path = require('path');
 
 module.exports = {
-  priority: 1000,
+  priority: 100,
   execute(app) {
     app.use(favicon(path.join(__dirname, './../public', 'favicon.ico')));
   },
